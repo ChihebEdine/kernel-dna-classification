69e2c02a5fadd284060e7bbc4c10aeec3c5f2ddc
Chiheb <chihebnajjar22@gmail.com>
2021-03-22 02:00:49 +0100

    add svm test and fix dimensions in tests

--- test_models.py
+++ test_models.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from models import KernelRidgeRegression, KernelLogisticRegression
+from models import KernelRidgeRegression, KernelLogisticRegression, KernelSVM
 from kernels import linear_kernel, gaussian_kernel
 
 
@@ -16,6 +16,8 @@ def test_KernelRidgeRegression(k="gaussian"):
 
     X = np.linspace(-np.pi, np.pi, 1000).reshape(1000, 1)
     y = 3 * np.sin(X) + np.random.normal(0, 0.3, size=(1000,1))
+    y = y.reshape(-1)
+
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
     
     KRR.train(X_train, y_train)
@@ -38,6 +40,8 @@ def test_KernelLogisticRegression(k="gaussian"):
 
     X = np.linspace(-np.pi, np.pi, 1000).reshape(1000, 1)
     y = 2*(X>0).astype(int) - 1
+    y = y.reshape(-1)
+    
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
     
     KLR.train(X_train, y_train)
@@ -49,8 +53,37 @@ def test_KernelLogisticRegression(k="gaussian"):
     plt.show()
 
 
+def test_KernelSVM(k="gaussian"):
+    if k == "gaussian":
+        SVM = KernelSVM(gaussian_kernel, Lambda=0.01)
+    elif k == "linear":
+        SVM = KernelSVM(linear_kernel, Lambda=0.01)
+    else:
+        return
+    
+    X = np.random.uniform(low=-1.0, high=1.0, size=(100,2))
+    y = 2*(X[:,0]>0).astype(int) - 1
+    color = ["blue"  if i==1 else "red" for i in y ]
+    plt.scatter(X[:,0], X[:,1], c=color)
+    plt.title(f"Ground truth coloring")
+    plt.show()
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+    SVM.train(X_train, y_train)
+
+    y_pred = SVM.predict(X_test)
+    c = ["blue"  if i==1 else "red" for i in y_pred ]
+    plt.scatter(X_test[:,0], X_test[:,1], c=c)
+    plt.title(f"Kernel Support Vector Machines ({k})")
+    plt.legend()
+    plt.show()
+
+
+
 if __name__ == "__main__":
     test_KernelRidgeRegression(k="gaussian")
     test_KernelRidgeRegression(k="linear")
     test_KernelLogisticRegression(k="gaussian")
     test_KernelLogisticRegression(k="linear")
+    test_KernelSVM(k="gaussian")
+    test_KernelSVM(k="linear")
