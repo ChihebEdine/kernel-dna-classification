251aafdeb515105be272a08f3724ad8bf3bf0116
Chiheb <chihebnajjar22@gmail.com>
2021-03-24 20:36:52 +0100

    improve train_predict function

--- start.py
+++ start.py
@@ -1,26 +1,28 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
 from kernels import spectrum_kernel, k_substrings_embedding
 from models import KernelSVM
-from utils import accuracy
+import numpy as np
 
 
-def train_and_predict_advanced(model, threshold, k, dataset=0):
-    
-    X_seq = pd.read_csv(f"data/train/Xtr{dataset}.csv")
-    X = X_seq["seq"].map(lambda s : k_substrings_embedding(s, k=k)).values
+def train_and_predict(model, threshold, k=None, dataset=0):
+
+    if k is None:
+        X = np.genfromtxt(f"data/train/Xtr{dataset}_mat100.csv", delimiter=' ')
+    else:
+        X_seq = pd.read_csv(f"data/train/Xtr{dataset}.csv")
+        X = X_seq["seq"].map(lambda s : k_substrings_embedding(s, k=k)).values
+
     y = pd.read_csv(f"data/train/Ytr{dataset}.csv")["Bound"].values
     y = 2*y - 1
     
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    
-    model.train(X_train, y_train)
-    
-    y_pred, _ = model.predict(X_test, threshold=threshold)
-    print(f"[Data set {dataset}] Test accuracy = ", accuracy(y_test, y_pred))
-    
+    model.train(X, y)
+
     X_seq_te = pd.read_csv(f"data/test/Xte{dataset}.csv")
-    X_te = X_seq_te["seq"].map(lambda s : k_substrings_embedding(s, k=k)).values
+    if k is None:
+        X_te = np.genfromtxt(f"data/test/Xte{dataset}_mat100.csv", delimiter=' ')
+    else:
+        X_te = X_seq_te["seq"].map(lambda s : k_substrings_embedding(s, k=k)).values
+
     y_pred_te, _ = model.predict(X_te, threshold=threshold)
     
     X_seq_te["Bound"] = (y_pred_te+1)//2
@@ -42,9 +44,9 @@ if __name__ == "__main__":
     threshold2 = 0.14
     k2 = 9
 
-    X_seq_te0 = train_and_predict_advanced(model=model0, threshold=threshold0, k=k0, dataset=0)
-    X_seq_te1 = train_and_predict_advanced(model=model1, threshold=threshold1, k=k1, dataset=1)
-    X_seq_te2 = train_and_predict_advanced(model=model2, threshold=threshold2, k=k2, dataset=2)
+    X_seq_te0 = train_and_predict(model=model0, threshold=threshold0, k=k0, dataset=0)
+    X_seq_te1 = train_and_predict(model=model1, threshold=threshold1, k=k1, dataset=1)
+    X_seq_te2 = train_and_predict(model=model2, threshold=threshold2, k=k2, dataset=2)
 
     FINAL = pd.concat([X_seq_te0, X_seq_te1, X_seq_te2], ignore_index=True)[["Id", "Bound"]]
     FINAL.to_csv("data/final_submission.csv", index=False)
