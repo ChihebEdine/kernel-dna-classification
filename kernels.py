c57bed2d9b60e35fdc4a82da9e72e44350f91c03
Chiheb <chihebnajjar22@gmail.com>
2021-03-24 20:33:37 +0100

    add doc to kernels.py

--- kernels.py
+++ kernels.py
@@ -2,11 +2,37 @@ import numpy as np
 
 
 def linear_kernel(X_left, X_right):
+    """ 
+    The gram matrix for a linear kernel
+    
+    Parameters
+    ----------
+        X_left : np.array of shape (n_left, m)
+        X_right : np.array of shape (n_right, m)
+
+    Returns
+    -------
+        K : gram matrix of shape (n_left, n_right)
+            K_ij = Inner_Product(X_left_i, X_right_j)
+    """
     return X_left @ X_right.T
 
 #===============================================================
 
 def gaussian_kernel(X_left, X_right, sigma2=1):
+    """ 
+    The gram matrix for a gaussian kernel
+    
+    Parameters
+    ----------
+        X_left : np.array of shape (n_left, m)
+        X_right : np.array of shape (n_right, m)
+
+    Returns
+    -------
+        K : gram matrix of shape (n_left, n_right)
+            K_ij = Gauss_Kernel(X_left_i, X_right_j)
+    """
     diff = (X_left[:, None, :] - X_right[None, : , :])**2
     norm2 = diff.sum(axis=2)
     return np.exp(-norm2/sigma2)
@@ -14,6 +40,23 @@ def gaussian_kernel(X_left, X_right, sigma2=1):
 #===============================================================
 
 def k_substrings_embedding(s, k=3):
+    """
+    computes an embedding for a given string s
+    number of occurences of all k substrings of s
+
+    Parameters
+    ----------
+        s : str
+            input string
+        k : int
+            number of charaters to consider
+
+    Returns
+    -------
+        Phi : dict (spectrum embedding of s)
+            the keys are k substrings of s
+            the values are the number of occurences of the keys in s
+    """
     phi = {}
     for i in range(len(s)-k+1):
         sub = s[i:i+k]
@@ -24,6 +67,23 @@ def k_substrings_embedding(s, k=3):
     return phi
    
 def seq_inner_product(phi1, phi2):
+    """
+    computes the inner product between two given 
+    spectrum embeddings
+
+    Parameters
+    ----------
+        phi1 : dict
+            a spectrum embeddings
+        phi2 : dict
+            a spectrum embeddings
+
+    Returns
+    -------
+        p : int
+            p = sum_a phi1(a)*phi2(a) 
+            where a is a commun substring of phi1 and phi2
+    """
     p = 0
     for sub in phi1:
         if sub in phi2:
@@ -31,6 +91,19 @@ def seq_inner_product(phi1, phi2):
     return p
 
 def spectrum_kernel(X_left, X_right):
+    """ 
+    The gram matrix for the spectrum kernel
+    
+    Parameters
+    ----------
+        X_left : np.array of n_left dicts (spectrum embeddings)
+        X_right : np.array of n_right dicts (spectrum embeddings)
+
+    Returns
+    -------
+        K : gram matrix of shape (n_left, n_right)
+            K_ij = Spectrum_Kernel(X_left_i, X_right_j)
+    """
     n1 = X_left.shape[0]
     n2 = X_right.shape[0]
     K = np.zeros((n1, n2))
